87dbb379ca948ddd293ef661238f556ba5b4e44e
RenanSchmitt <renanschmitt.ti@gmail.com>
2019-05-14 21:18:56 -0300

    3

--- inc.home.php
+++ inc.home.php
@@ -1,5 +1,99 @@
 <h1>
- A maior liga/loja de Gravataí de Magic: The Gathering.
+
 </h1>
 
-<img src="./images/carossel1.jpg" width="70%" height="500">
+<img src="./images/carossel1.jpg" width="60%" height="400">
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Magic NO LIMITS</title>
+        <link rel="shortcut icon" href="../images/fav_icon.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <!-- Bulma Version 0.7.4-->
+        <link rel="stylesheet" href="https://unpkg.com/bulma@0.7.4/css/bulma.min.css" />
+        <link rel="stylesheet" type="text/css" href="../css/hero.css">
+        <link rel="stylesheet" href="https://unpkg.com/bulma-modal-fx/dist/css/modal-fx.min.css" />
+    </head>
+    <body>
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        A maior liga/loja de Gravataí de Magic: The Gathering.
+                    </h1>
+                    <h2 class="subtitle">
+                    Seja bem-vindo a maior loja de Magic: The Gathering de Gravataí, aqui você irá encontrar tudo que será necessario para você se tornar o melhor!
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <div class="box cta">
+            <p class="has-text-centered">
+                <span class="tag is-primary">New</span> Fique ligado, todos os dias nós temos as melhores promoções do mercado!
+            </p>
+        </div>
+        <section class="container">
+            <div class="columns features">
+                <div class="column is-4">
+                    <div class="card is-shady">
+                        <div class="card-image has-text-centered">
+                            <i class="fa fa-paw"></i>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <h4>Lista de produtos. </h4>
+                                <p>Nós temos o maior acervo da região</p>
+                                <p><a href="index.php?pg=produtos">Clique aqui</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-4">
+                    <div class="card is-shady">
+                        <div class="card-image has-text-centered">
+                            <i class="fa fa-empire"></i>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <h4>Lista de fornecedores.</h4>
+                                <p>Saiba quem são nossos fornecedores!</p>
+                                <p><a href="index.php?pg=fornecedores">Clique aqui</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column is-4">
+                    <div class="card is-shady">
+                        <div class="card-image has-text-centered">
+                            <i class="fa fa-apple"></i>
+                        </div>
+                        <div class="card-content">
+                            <div class="content">
+                                <h4> Filiais. </h4>
+                                <p>Saiba onde nos enconttrar.</p>
+                                <p><a href="index.php?pg=filiais">Clique aqui</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="intro column is-8 is-offset-2">
+                <h2 class="title">Magic NO LIMITS</h2><br>
+                <p class="subtitle has-text-centered">Conte sempre com nós!</p>
+            </div>
+        
+        <footer class="footer">
+            <div class="container has-centered">
+                <div class="columns">
+                  
+   
+
+                </div>
+
+            <script src="../js/bulma.js"></script>
+        </footer>
+    </body>
+</html>
